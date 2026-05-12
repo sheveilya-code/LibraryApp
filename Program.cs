4eed43a39b05a1b4ec700755b2aa704ba4d60e5d
Илья <sheveilya@bk.ru>
2026-05-12 19:57:46 +0500

    Lab 2: Implemented encapsulation, properties, constructor with validation

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
 ﻿using LibraryApp.Models;
+using System;
 
-Book myBook = new Book();
-myBook.Title = "1984";
-myBook.Author = "Джордж Оруэлл";
-myBook.Year = 1949;
-myBook.DisplayInfo();
+namespace LibraryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Book book1 = new Book("1984", "Дж. Оруэлл", 1949);
+                Book book2 = new Book("Гарри Поттер", "Дж. Роулинг", 1997);
+                Book book3 = new Book("Мастер и Маргарита", "М. Булгаков", 1967);
+
+                book1.DisplayInfo();
+                book2.DisplayInfo();
+                book3.DisplayInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
